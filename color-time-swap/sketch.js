494d6bd5f430cf71f116704a69ec0701a2ccc1d4
samIam1507 <samuelwardell07@gmail.com>
2025-02-25 14:09:32 -0600

    made the window full screen

--- color-time-swap/sketch.js
+++ color-time-swap/sketch.js
@@ -3,18 +3,11 @@ let timeLastSwitched = 0;
 let isWhite = true;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(220);
-  // console.log(millis());
-  // if ((floor(millis() / someTime)) % 2 === 0) {
-  //   background("white");
-  // }
-  // else {
-  //   background("black");  
-  // }
   if (millis() > someTime + timeLastSwitched) {
     isWhite = !isWhite;
     timeLastSwitched = millis();
