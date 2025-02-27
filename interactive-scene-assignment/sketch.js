61ebeeb6d3d8827b3da57afa4d32367c0549e3cf
samIam1507 <samuelwardell07@gmail.com>
2025-02-27 12:10:42 -0600

    traffic lights and updated snake

--- interactive-scene-assignment/sketch.js
+++ interactive-scene-assignment/sketch.js
@@ -5,71 +5,78 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-const speed = 20;
+const speed = 5;
 let direction = "right";
 let locationList = [];
 let dx;
-let dy;
+let dy = 0;
 let appleJustEaten = true;
 let appleX;
 let appleY;
 let x = 50;
 let y = 50;
+let addToList = false;
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   dx = speed;
-  dy = speed;
-  background(220);
 }
 
 function draw() {
+  background(220);
   fill(40, 40, 130);
-  // setDirection();
-  // snakeHeadMotion();
+  setDirection();
+  snakeHeadMotion();
   move();
   eatApple();
   createApple();
-  // checkIfLengthening();
-  // drawBody(); 
+  drawBody(); 
 }
 
-// function setDirection() {
-//   if (keyIsDown(87)) {
-//     direction = "up";
-//   }
-//   else if (keyIsDown(68)) {
-//     direction = "right";
-//   }
-//   else if (keyIsDown(83)) {
-//     direction = "down";
-//   }
-//   else if (keyIsDown(65)) {
-//     direction = "left";
-//   }
-// }
+function setDirection() {
+  if (keyIsDown(87)) {
+    direction = "up";
+  }
+  else if (keyIsDown(68)) {
+    direction = "right";
+  }
+  else if (keyIsDown(83)) {
+    direction = "down";
+  }
+  else if (keyIsDown(65)) {
+    direction = "left";
+  }
+}
 
-// function snakeHeadMotion() {
-//   if (x % 10 === 0 && y % 10 === 0) {
-//     if (direction === "right") {
-//       dx = speed;
-//       dy = 0;
-//     }
-//     if (direction === "down") {
-//       dy = speed;
-//       dx = 0;
-//     }
-//     if (direction === "left") {
-//       dx = -speed;
-//       dy = 0;
-//     }
-//     if (direction === "up") {
-//       dy = -speed;
-//       dx = 0;
-//     }
-//   }
-// }
+function snakeHeadMotion() {
+  if (x % 25=== 0 && y % 25=== 0) {
+    if (direction === "right") {
+      dx = speed;
+      dy = 0;
+      addToList = true;
+    }
+    if (direction === "down") {
+      dy = speed;
+      dx = 0;
+      addToList = true;
+    }
+    if (direction === "left") {
+      dx = -speed;
+      dy = 0;
+      addToList = true;
+    }
+    if (direction === "up") {
+      dy = -speed;
+      dx = 0;
+      addToList = true;
+    }
+    if (addToList) {
+      locationList.concat[x, y];
+      addToList = false;
+    }
+  }
+}
 
 function move() {
   x += dx;
@@ -79,24 +86,20 @@ function move() {
 }
 
 
-// function drawBody() {
-//   for (let coordinates of locationList){
-//     let placeholderX = coordinates.pop(0);
-//     let placeholderY = coordinates.pop(0);
-//     circle(placeholderX, placeholderY, 25);
-//   }
-// }
+function drawBody() {
+  for (let coordinates of locationList) {
+    let placeholderX = coordinates[0];
+    let placeholderY = coordinates[1];
+    fill(0, 255, 0);
+    circle(placeholderX, placeholderY, 25);
+  }
+}
 
-// function checkIfLengthening() {
-//   if (!appleJustEaten) {
-//     locationList.append([x, y]);
-//   }
-// }
 
 function createApple() {
   if (appleJustEaten) {
-    appleX = floor(random(windowWidth) / 5) * 5;
-    appleY = floor(random(windowHeight) / 5) * 5;
+    appleX = floor(random(windowWidth) / 25) * 25;
+    appleY = floor(random(windowHeight) / 25) * 25;
     appleJustEaten = false;
   }
   fill(130, 40, 40);
