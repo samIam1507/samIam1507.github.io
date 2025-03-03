cb09f0c46687f01c6d1b461c75be461866368010
samIam1507 <samuelwardell07@gmail.com>
2025-03-03 14:15:36 -0600

    finally it works ishhh

--- interactive-scene-assignment/sketch.js
+++ interactive-scene-assignment/sketch.js
@@ -7,7 +7,7 @@
 
 const speed = 5;
 let direction = "right";
-let locationList = [[49, 50][50, 50]];
+let locationList = [49, 50, 50, 50];
 let dx;
 let dy = 0;
 let appleJustEaten = true;
@@ -19,35 +19,38 @@ let placeholderList;
 let placeholderX = 50;
 let placeholderY = 50;
 let overlap = false;
+let lengthVar = 3;
+let dead = false;
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   dx = speed;
+  noStroke();
 }
 
 function draw() {
   background(220);
   fill(40, 40, 130);
   setDirection();
-  eatApple();
   snakeHeadMotion();
   move();
   drawBody(); 
   createApple();
+  isDead();
 }
 
 function setDirection() {
-  if (keyIsDown(87) && !(direction === "down")) {
+  if (keyIsDown(87) && direction !== "down") {
     direction = "up";
   }
-  else if (keyIsDown(68) && !(direction === "left")) {
+  else if (keyIsDown(68) && direction !== "left") {
     direction = "right";
   }
-  else if (keyIsDown(83) && !(direction === "up")) {
+  else if (keyIsDown(83) && direction !== "up") {
     direction = "down";
   }
-  else if (keyIsDown(65) && !(direction === "right")) {
+  else if (keyIsDown(65) && direction !== "right") {
     direction = "left";
   }
 }
@@ -70,6 +73,7 @@ function snakeHeadMotion() {
       dy = -speed;
       dx = 0;
     }
+    eatApple();
     recordCoordinates();
   }
 }
@@ -83,11 +87,8 @@ function move() {
 
 function drawBody() {
   fill(0, 255, 0);
-  for (let i = 0; i < locationList.length; i++) {
-    placeholderList = locationList[i];
-    placeholderX = placeholderList[0];
-    placeholderY = placeholderList[1];
-    circle(placeholderX, placeholderY, 25);
+  for (let i = 0; i < locationList.length; i += 2) {
+    circle(locationList[i], locationList[i + 1], 25);
   }
 }
 
@@ -96,10 +97,12 @@ function createApple() {
   if (appleJustEaten) {
     appleX = floor(random(5, windowWidth / 25 - 5)) * 25;
     appleY = floor(random(5, windowHeight / 25 - 5)) * 25;
-    for (let i = 0; i < locationList.length; i++) {
-      let placeholderList = locationList[i];
-      if (appleX === placeholderList[0] && appleY === placeholderList[1]) {
+    for (let i = 0; i < locationList.length; i += 2) {
+      if (appleX === locationList[i] && appleY === locationList[i+1]) {
         overlap = true;
+      }
+      if (x === locationList[1] && y === locationList[i + 1]) {
+        dead = true;
       }
     }
     if (!overlap) {
@@ -108,24 +111,26 @@ function createApple() {
   }
   fill(130, 40, 40);
   circle(appleX, appleY, 25);
-  console.log(appleX, appleY);
   overlap = false;
 }
 
 function eatApple() {
   if (x === appleX && y === appleY) {
     appleJustEaten = true;
+    lengthVar += 1;
   }
 }
 
 function recordCoordinates() {
-  locationList = concat(locationList + [[x, y]]);
-  // if (!appleJustEaten) {
-  //   for (let i = 0; i < locationList; i++) {
-  //     if (i > 0) {
-  //       placeholderList += locationList[i];
-  //     }
-  //   }
-  //   locationList = placeholderList;
-  // }
+  locationList = concat([x, y], locationList);
+  if (lengthVar < locationList.length / 2) {
+    locationList.pop();
+    locationList.pop();
+  }
+}
+
+function isDead() {
+  if (dead) {
+    console.log("dead");
+  }
 }
