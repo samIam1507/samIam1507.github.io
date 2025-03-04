8db4a9cd4fab8149106c4059aaece202893f1541
samIam1507 <samuelwardell07@gmail.com>
2025-03-04 14:15:22 -0600

    worked on death

--- interactive-scene-assignment/sketch.js
+++ interactive-scene-assignment/sketch.js
@@ -29,6 +29,8 @@ let obstacleLocationList = [];
 let obstacleX;
 let obstacleY;
 let obstacleNeeded = false;
+let millisDeathDelay = millis(5);
+let millisDeathRecorded;
 
 // function preload() {
 //   setting = Image("setting_button.png");
@@ -44,6 +46,7 @@ function draw() {
   setMode();
   mode();
   fill(40, 40, 130);
+  obstacle();
   setDirection();
   snakeHeadMotion();
   move();
@@ -51,7 +54,6 @@ function draw() {
   createApple();
   isDead();
   scrolling();
-  obstacle();
 }
 
 function setMode() {
@@ -60,10 +62,10 @@ function setMode() {
   }
   if (activeMode === "mode-selection" && mouseX > windowWidth / 2 - 20 && mouseX < windowWidth / 2 + 80 && mouseIsPressed) {
     if (mouseY < windowHeight + scrollingPositionChange && mouseY > windowHeight - 20 + scrollingPositionChange) {
-      activeMode = "obstacle"
+      activeMode = "obstacle";
     }
     if (mouseY < windowHeight + scrollingPositionChange + 60 && mouseY > windowHeight + 40 + scrollingPositionChange) {
-      activeMode = "play-basic"
+      activeMode = "play-basic";
     }
   }
 }
@@ -76,15 +78,15 @@ function mode() {
     text("Game Mode Selection", windowWidth / 2 - 200, windowHeight / 2 - 50);
   }
   if (activeMode === "mode-selection") {
-    fill("green")
+    fill("green");
     textSize(30);
     stroke(0);
     strokeWeight(4);
     text("(Scroll for More Options, Clic to Stop Scrolling)", windowWidth / 2 - 200, windowHeight / 4);
-    fill("yellow")
-    text("Obstacle Mode", windowWidth / 2 - 20, windowHeight + scrollingPositionChange)
+    fill("yellow");
+    text("Obstacle Mode", windowWidth / 2 - 20, windowHeight + scrollingPositionChange);
     fill("blue");
-    text("Basic Mode", windowWidth / 2 - 20, windowHeight + 60 + scrollingPositionChange)
+    text("Basic Mode", windowWidth / 2 - 20, windowHeight + 60 + scrollingPositionChange);
   }
   if (activeMode === "play-basic") {
     direction = "right";
@@ -93,7 +95,7 @@ function mode() {
     y = 50;
     placeholderX = 50;
     placeholderY = 50;
-    activeMode = "ongoing"
+    activeMode = "ongoing";
     appleJustEaten = true;
     noStroke();
   }
@@ -104,7 +106,7 @@ function mode() {
     y = 50;
     placeholderX = 50;
     placeholderY = 50;
-    activeMode = "ongoing"
+    activeMode = "ongoing";
     appleJustEaten = true;
     obstacleMode = true;
     obstacleNeeded = true;
@@ -115,6 +117,14 @@ function mode() {
     locationList = [];
     x = NaN;
     y = NaN;
+    obstacleLocationList = [];
+    appleX = NaN;
+    appleY = NaN;
+    millisDeathRecorded = millis();
+    activeMode = "reloading";
+  }
+  if (activeMode === "reloading") {
+    activeMode = "start";
   }
 }
 
@@ -173,9 +183,6 @@ function drawBody() {
 
 function createApple() {
   if (appleJustEaten) {
-    if (obstacleMode) {
-
-    }
     appleX = floor(random(5, windowWidth / 25 - 5)) * 25;
     appleY = floor(random(5, windowHeight / 25 - 5)) * 25;
     for (let i = 2; i < locationList.length; i += 2) {
@@ -219,16 +226,16 @@ function isDead() {
       dead = true; 
     }
   }
-  for (let n = 0; n < obstacleLocationListLocationList.length; n += 2) {
-    // if (x === obstacleLocationList[n] && y === obstacleLocationList[n + 1]) {
-    //   dead = true;
-    // }
+  for (let n = 0; n < obstacleLocationList.length; n += 2) {
+    if (x === obstacleLocationList[n] && y === obstacleLocationList[n + 1]) {
+      dead = true;
+    }
   }
   if (x <= 12.5 || x >= windowWidth - 12.5 || y <= 0 || y >= windowHeight - 12.5) {
     dead = true;
   }
   if (dead) {
-    activeMode = "dead"
+    activeMode = "dead";
   }
 }
 
@@ -262,9 +269,9 @@ function obstacle() {
     obstacleX = floor(random(5, windowWidth / 25 - 5)) * 25;
     obstacleY = floor(random(5, windowHeight / 25 - 5)) * 25;
     for (let i = 0; i < locationList.length; i +=2) {
-     if (obstacleX === locationList[i] && obstacleY === locationList[i + 1]) {
-      obstacleNeeded = true;
-     }
+      if (obstacleX === locationList[i] && obstacleY === locationList[i + 1]) {
+        obstacleNeeded = true;
+      }
     }
     for (let n = 0; n < obstacleLocationList.length; n += 2) {
       if (obstacleX === obstacleLocationList[n] && obstacleY === obstacleLocationList[n + 1]) {
@@ -275,10 +282,11 @@ function obstacle() {
       obstacleNeeded = true;
     }
     if (!obstacleNeeded) {
-      obstacleLocationList = concat([obstacleX, obstacleY], obstacleLocationList);
+      obstacleLocationList.push(obstacleX);
+      obstacleLocationList.push(obstacleY);
     }
   }
   for (let b = 0; b < obstacleLocationList.length; b += 2) {
-    square(obstacleLocationList[b] + 12.5, obstacleLocationList[b + 1] + 12.5, 25);
+    square(obstacleLocationList[b] - 12.5, obstacleLocationList[b + 1] - 12.5, 25);
   }
 }
