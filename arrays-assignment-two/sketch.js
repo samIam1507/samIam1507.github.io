e9e042b3a45c653c37731d4c0b1da67dbef3454f
samIam1507 <samuelwardell07@gmail.com>
2025-03-12 14:17:42 -0600

    early poker? work

--- arrays-assignment-two/sketch.js
+++ arrays-assignment-two/sketch.js
@@ -5,6 +5,11 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let deck = [];
+let myHand = [];
+let flop = [];
+let turn = [];
+let river = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,4 +17,16 @@ function setup() {
 
 function draw() {
   background(220);
+}
+
+function createDeck() {
+  let card = {
+    number: 1,
+    suit: 1,
+  };
+  for (let cardNumber = 1; cardNumber < 14; cardNumber++) {
+    for (let suit = 1; suit < 5; suit ++) {
+      
+    }
+  }
 }
