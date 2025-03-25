db9f324de248679faa37739a44115c2f8f3569b5
samIam1507 <samuelwardell07@gmail.com>
2025-03-25 14:06:45 -0600

    game of life demo

--- game-of-life-demo/sketch.js
+++ game-of-life-demo/sketch.js
@@ -1,65 +1,105 @@
-// Game of Life Demo
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
 
-let cellSize;
-const SQUARE_DIMENSIONS = 20;
-let grid;
-let mouseDown = false;
+const CELL_SIZE = 20;
+let heightFactor;
+let widthFactor;
+let grid = [[]];
+let autoPlayIsOn = false;
+let gosper;
+
+function preload() {
+  gosper = loadJSON("gosper-gun.json");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  grid = generateRandomGrid(SQUARE_DIMENSIONS, SQUARE_DIMENSIONS);
-  if (height > width) {
-    cellSize = width / SQUARE_DIMENSIONS;
-  }
-  else {
-    cellSize = height / SQUARE_DIMENSIONS;
-  }
+  heightFactor = Math.floor(height / CELL_SIZE);
+  widthFactor = Math.floor(width / CELL_SIZE);
+  grid = generateRandomGrid(widthFactor, heightFactor);
 }
 
 function draw() {
   background(220);
-  displayGrid();
-}
-function keyPressed() {
-  if (key === "r") {
-    grid = generateRandomGrid(SQUARE_DIMENSIONS, SQUARE_DIMENSIONS);
+  if (autoPlayIsOn) {
+    grid = updateGrid();
   }
-  else if (key === "e") {
-    grid = generateGrid(SQUARE_DIMENSIONS, SQUARE_DIMENSIONS);
-  }
+  drawGrid();
 }
 
-function displayGrid() {
-  for (let y = 0; y < SQUARE_DIMENSIONS; y ++) {
-    for (let x = 0; x < SQUARE_DIMENSIONS; x ++) {
+function updateGrid() {
+  newGrid = generateGrid(widthFactor, heightFactor);
+  for (let y = 0; y < heightFactor; y ++) {
+    for (let x = 0; x < widthFactor; x ++) {
+      let counter = 0;
+      for (let i = - 1; i < 2; i ++){
+        for (let n = - 1; n < 2; n ++) {
+          if (x + n >= 0 && x + n < widthFactor && y + i >= 0 && y + i < heightFactor ) {
+            if (grid[y + i][x + n]) {
+              counter ++;
+            }
+          }
+        }
+      }
       if (grid[y][x]) {
-        fill("white");
+        counter -= 1;
+        if (counter < 2 || counter > 3) {
+          newGrid[y][x] = false;
+        }
+        else {
+          newGrid[y][x] = true;        
+        }
       }
       else {
-        fill("black");
+        if (counter === 3) {
+          newGrid[y][x] = true;
+        }
+        else {
+          newGrid[y][x] = false;
+        }
       }
-      rect(x * cellSize, y * cellSize, cellSize, cellSize);
-    }
-  }
-}
-
-function generateGrid(cols, rows) {
-  let newGrid = [];
-  for (let y = 0; y < rows; y ++) {
-    newGrid.push([]);
-    for (let x = 0; x < cols; x ++) {
-      newGrid[y].push(true);
     }
   }
   return newGrid;
 }
 
-function generateRandomGrid(cols, rows) {
+function generateGrid(rows, cols) {
   let newGrid = [];
-  for (let y = 0; y < rows; y ++) {
+  for (let y = 0; y < cols; y ++) {
     newGrid.push([]);
-    for (let x = 0; x < cols; x ++) {
-      if (random(100) < 50) {
+    for (let x = 0; x < rows; x ++) {
+      newGrid[y].push(false);
+    }
+  }
+  return newGrid;
+}
+
+function keyPressed() {
+  if (key === "e") {
+    grid = generateGrid(widthFactor, heightFactor);
+  }
+  else if (key === "r") {
+    grid = generateRandomGrid(widthFactor, heightFactor);
+  }
+  else if (key === "p") {
+    grid = updateGrid();
+  }
+  else if (key === "g") {
+    grid = gosper;
+  }
+}
+
+function generateRandomGrid(rows, cols) {
+  let newGrid = [];
+  for (let y = 0; y < cols; y ++) {
+    newGrid.push([]);
+    for (let x = 0; x < rows; x ++) {
+      let color = random(100);
+      if (color > 50) {
         newGrid[y].push(true);
       }
       else {
@@ -69,20 +109,33 @@ function generateRandomGrid(cols, rows) {
   }
   return newGrid;
 }
-
-function toggle(x, y) {
-  if (x >= 0 && x < SQUARE_DIMENSIONS && y >= 0 && y < SQUARE_DIMENSIONS) {
-    grid[y][x] = !grid[y][x];
+function drawGrid() {
+  for (let y = 0; y < heightFactor; y ++) {
+    for (let x = 0; x < widthFactor; x ++) {
+      if (grid[y][x]) {
+        fill("black");
+      }
+      else {
+        fill("white");
+      }
+      square(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE);
+    }
   }
 }
 
 function mousePressed() {
-  let x = Math.floor(mouseX/cellSize);
-  let y = Math.floor(mouseY/cellSize);
-  
+  let x = Math.floor(mouseX / CELL_SIZE);
+  let y = Math.floor(mouseY / CELL_SIZE);
+
   toggle(x, y);
-  toggle(x + 1, y);
-  toggle(x, y + 1);
-  toggle(x - 1, y);
-  toggle(x, y - 1);
+  // toggle(x + 1, y);
+  // toggle(x, y + 1);
+  // toggle(x - 1, y);
+  // toggle(x, y - 1);
+}
+
+function toggle(x, y) {
+  if (x >= 0 && x < widthFactor && y >= 0 && y < heightFactor) {
+    grid[y][x] = !grid[y][x];
+  }
 }
--- terain-demo/sketch.js
+++ terain-demo/sketch.js
@@ -1,40 +1,82 @@
-// Terain Generation Demo
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
 
-let terrain = [];
-const NUMBER_OF_RECTS = 40000;
+const CELL_SIZE = 200;
+let heightFactor;
+let widthFactor;
+let grid = [[]];
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  generateTerrain(width / NUMBER_OF_RECTS);
+  heightFactor = Math.floor(height / CELL_SIZE);
+  widthFactor = Math.floor(width / CELL_SIZE);
+  grid = generateGrid(widthFactor, heightFactor);
 }
 
 function draw() {
   background(220);
-  stroke('green');
-  fill('green');
+  drawGrid();
+}
 
-  for (let someRect of terrain) {
-    rect(someRect.x, someRect.y, someRect.w, someRect.h);
+function generateGrid(rows, cols) {
+  let newGrid = [];
+  for (let y = 0; y < cols; y ++) {
+    newGrid.push([]);
+    for (let x = 0; x < rows; x ++) {
+      newGrid[y].push(true);
+    }
+  }
+  return newGrid;
+}
+
+function generateRandomGrid(rows, cols) {
+  let newGrid = [];
+  for (let y = 0; y < cols; y ++) {
+    newGrid.push([]);
+    for (let x = 0; x < rows; x ++) {
+      let color = random(100);
+      if (color > 50) {
+        newGrid[y].push(true);
+      }
+      else {
+        newGrid[y].push(false);
+      }
+    }
+  }
+  return newGrid;
+}
+function drawGrid() {
+  for (let y = 0; y < heightFactor; y ++) {
+    for (let x = 0; x < widthFactor; x ++) {
+      if (grid[y][x]) {
+        fill("white");
+      }
+      else {
+        fill("black");
+      }
+      square(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE);
+    }
   }
 }
 
-function spawnRectangle(leftSide, rectHeight, rectWidth) {
-  let theRect = {
-    x: leftSide,
-    y: height - rectHeight,
-    w: rectWidth,
-    h: rectHeight,
-  };
+function mousePressed() {
+  let x = Math.floor(mouseX / CELL_SIZE);
+  let y = Math.floor(mouseY / CELL_SIZE);
 
-  return theRect;
+  toggle(x, y);
+//   toggle(x + 1, y);
+//   toggle(x, y + 1);
+//   toggle(x - 1, y);
+//   toggle(x, y - 1);
 }
 
-function generateTerrain(widthOfRect) {
-  let time = 0;
-  let deltaTime = 0.00005;
-  for (let i = 0; i < NUMBER_OF_RECTS; i++) {
-    time += deltaTime;
-    let theHeight = noise(time) * height;
-    terrain.push(spawnRectangle(i * widthOfRect, theHeight, widthOfRect));
+function toggle(x, y) {
+  if (x >= 0 && x < widthFactor && y >= 0 && y < heightFactor) {
+    grid[y][x] = !grid[y][x];
   }
 }
