b9707192f7ba135ea231faddc3e280d180c06bf5
samIam1507 <samuelwardell07@gmail.com>
2025-04-08 20:37:07 -0600

    start of avalanch mode and gradual movement

--- two-d-arrays-assignment/sketch.js
+++ two-d-arrays-assignment/sketch.js
@@ -11,12 +11,20 @@ let cols = 8;
 let playerOnePlaying = true;
 let extraTurn = false;
 let invalidSize = false;
+let gameMode = "avalanch";
+let x; 
+let y;
+let stonesMovedCounter;
 
 let sumTop;
 let sumBottom;
 let scaleFactor;
 let xPosition;
 let yPosition;
+let lastCounter;
+let counterConstant = 500;
+let stoneCounter;
+let callMovement = false;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -31,6 +39,36 @@ function setup() {
 function draw() {
   background(70, 150, 100);
   drawGrid();
+  callTileMovement();
+}
+
+function callTileMovement() {
+  if (lastCounter - counterConstant> millis() && stonesMovedCounter < stoneCounter) {
+    stonesMovedCounter += 1;
+    lastCounter = millis();
+    tileMovement();
+  }
+
+
+  for (let ix = 1; ix < 7; ix ++) {
+    sumTop += grid[0][ix];
+    sumBottom += grid[1][ix];
+  }
+  if (sumBottom === 0 || sumTop === 0) {
+    grid[0][0] += sumTop;
+    grid[1][cols - 1] += sumBottom;
+    for (let n = 0; n < 2; n ++) {
+      for (let j = 1; j < 7; j ++) {
+        grid[n][j] = 0;
+      }
+    }
+      
+  }
+
+  if (!extraTurn) {
+    playerOnePlaying = !playerOnePlaying;
+  }
+  extraTurn = false;
 }
 
 function createGrid() {
@@ -71,93 +109,58 @@ function drawGrid() {
 }
 
 function mousePressed() {
-  let y = Math.floor(mouseY / scaleFactor) - 1;
-  let x = Math.floor(mouseX / scaleFactor) - 1;
-
-
-  if (y === 0 && playerOnePlaying || y === 1 && !playerOnePlaying) {
-    moveTiles("basic", x, y);
+  if (!callMovement) {
+    y = Math.floor(mouseY / scaleFactor) - 1;
+    x = Math.floor(mouseX / scaleFactor) - 1;
+    stonesMovedCounter = 0;
+    stoneCounter = grid[y][x];
+    grid[y][x] = 0;
+  
+    if ((y === 0 && playerOnePlaying || y === 1 && !playerOnePlaying) && x > 0 && x < 8) {
+      callMovement = true;
+    }
   }
 }
 
-function moveTiles(gameMode, x, y) {
-  sumTop = 0;
-  sumBottom = 0;
-  let placeHolderX;
-  let placeHolderY;
-
-  if (gameMode === "basic") {
-    stoneCounter = grid[y][x];
-    grid[y][x] = 0;
-    
-    for (let i = 0; i < stoneCounter; i ++) {
-      if (y === 0) {
-        if (x !== 1) {
-          placeHolderX = x;
-          placeHolderY = y;
-          x --;
-          grid[y][x] ++;
-        }
-        else {
-          if (i + 1 === stoneCounter) {
-            extraTurn = true;
-          }
-          else {
-            y = 1;
-            x = 1;
-            grid[y][x] ++;
-          }
-          if (playerOnePlaying) {
-            grid[0][0] += 1;
-            i ++;
-          }
-        }
+function tileMovement() {
+  if (y === 0) {
+    if (x !== 1) {
+      x --;
+      grid[y][x] ++;
+    }
+    else {
+      if (stonesMovedCounter + 1 === stoneCounter) {
+        extraTurn = true;
       }
       else {
-        if (x !== cols - 2) {
-          x ++;
-          grid[y][x] ++;
-        }
-        else {
-          if (i + 1 === stoneCounter) {
-            extraTurn = true;
-          }
-          else {
-            y = 0;
-            x = cols - 2;
-            grid[y][x] ++;
-          }
-          if (!playerOnePlaying) {
-            i ++;
-            grid[1][cols - 1] += 1;
-          }
-        }
+        y = 1;
+        x = 1;
+        grid[y][x] ++;
+      }
+      if (playerOnePlaying) {
+        grid[0][0] += 1;
+        stonesMovedCounter ++;
       }
     }
-  
-    for (let ix = 1; ix < 7; ix ++) {
-      sumTop += grid[0][ix];
-      sumBottom += grid[1][ix];
-    }
-    if (sumBottom === 0 || sumTop === 0) {
-      grid[0][0] += sumTop;
-      grid[1][cols - 1] += sumBottom;
-      for (let n = 0; n < 2; n ++) {
-        for (let j = 1; j < 7; j ++) {
-          grid[n][j] = 0;
-        }
-      }
-        
-    }
-
-    if (!extraTurn) {
-      playerOnePlaying = !playerOnePlaying;
-    }
-    extraTurn = false;
   }
-
-  else if (gameMode === "avalanch") {
-    avalanchLoop(x, y);
+  else {
+    if (x !== cols - 2) {
+      x ++;
+      grid[y][x] ++;
+    }
+    else {
+      if (stonesMovedCounter + 1 === stoneCounter) {
+        extraTurn = true;
+      }
+      else {
+        y = 0;
+        x = cols - 2;
+        grid[y][x] ++;
+      }
+      if (!playerOnePlaying) {
+        grid[1][cols - 1] += 1;
+      }
+    }
   }
 }
 
@@ -169,8 +172,6 @@ function avalanchLoop(x, y) {
     for (let i = 0; i < stoneCounter; i ++) {
       if (y === 0) {
         if (x !== 1) {
-          placeHolderX = x;
-          placeHolderY = y;
           x --;
           grid[y][x] ++;
         }
@@ -209,6 +210,9 @@ function avalanchLoop(x, y) {
           }
         }
       }
+    }
+    if (grid[y][x] > 1 && x !== 0 && x !== 7) {
+      avalanchLoop(x, y);
     }
   }
 }
